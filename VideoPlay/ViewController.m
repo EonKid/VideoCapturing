d23790eb8959819879cb1fa8ab4a354a05d85a48
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-12-18 16:49:35 +0530

    Playback button Added

--- VideoPlay/ViewController.m
+++ VideoPlay/ViewController.m
@@ -20,6 +20,7 @@
 }
 
 - (IBAction)captureVideo:(id)sender {
+    
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         
         UIImagePickerController *picker = [[UIImagePickerController alloc] init];
@@ -35,15 +36,42 @@
     UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
         
     }];
+    
     [alertController addAction:alertAction];
     [self presentViewController:alertController animated:YES completion:nil];
 
 
 }
 
+
+- (IBAction)playbackButton:(id)sender {
+    
+    // pick a video from the documents directory
+    NSURL *video = [self grabFileURL:@"video.mov"];
+    
+    // create a movie player view controller
+    MPMoviePlayerViewController * controller = [[MPMoviePlayerViewController alloc]initWithContentURL:video];
+    [controller.moviePlayer prepareToPlay];
+    [controller.moviePlayer play];
+    
+    // and present it
+    [self presentMoviePlayerViewControllerAnimated:controller];
+}
+
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
     self.videoURL = info[UIImagePickerControllerMediaURL];
+    // grab our movie URL
+    NSURL *chosenMovie = [info objectForKey:UIImagePickerControllerMediaURL];
+    
+    // save it to the documents directory
+    NSURL *fileURL = [self grabFileURL:@"video.mov"];
+    NSData *movieData = [NSData dataWithContentsOfURL:chosenMovie];
+    [movieData writeToURL:fileURL atomically:YES];
+    
+    // save it to the Camera Roll
+    UISaveVideoAtPathToSavedPhotosAlbum([chosenMovie path], nil, nil, nil);
     [picker dismissViewControllerAnimated:YES completion:NULL];
     
     self.videoController = [[MPMoviePlayerController alloc] init];
@@ -52,14 +80,27 @@
     
     [self.videoController.view setFrame:CGRectMake (0, 0, [[UIScreen mainScreen] bounds].size.width , [[UIScreen mainScreen] bounds].size.height)];
     [self.view addSubview:self.videoController.view];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(videoPlayBackDidFinish:)
-                                                 name:MPMoviePlayerPlaybackDidFinishNotification
-                                               object:self.videoController];
-
     
+    [[NSNotificationCenter defaultCenter]
+                                        addObserver:self
+                                        selector:@selector(videoPlayBackDidFinish:)
+                                        name:MPMoviePlayerPlaybackDidFinishNotification
+                                        object:self.videoController
+     ];
+
     [self.videoController play];
     
+}
+
+- (NSURL*)grabFileURL:(NSString *)fileName {
+    
+    // find Documents directory
+    NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    
+    // append a file name to it
+    documentsURL = [documentsURL URLByAppendingPathComponent:fileName];
+    
+    return documentsURL;
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
@@ -75,7 +116,7 @@
     // Stop the video player and remove it from view
     [self.videoController pause];
     [self.videoController.view removeFromSuperview];
-    self.videoController = nil;
+     self.videoController = nil;
     
     // Display a message
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Video Playback" message:@"Just finished the video playback. The video is now removed." preferredStyle:UIAlertControllerStyleAlert];
