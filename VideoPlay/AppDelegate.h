1f5fbed6d6b8b149f4b9b109d35153a761bdb9e1
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-12-18 14:47:39 +0530

    Initial Commit

---
+++ VideoPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlay
+//
+//  Created by Aseem 1 on 18/12/15.
+//  Copyright (c) 2015 codeBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
